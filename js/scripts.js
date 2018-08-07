18dd88468f0481cbb51e34b320c5b67d87dd85f5
antony <kiariehjames@gmail.com>
2018-08-07 09:01:55 +0300

    Add a js css and README files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add = function (number1, number2){
+  return(number1 + number2);
+}
+var number1 = prompt(parseInt("Enter a number 1"));
+var number2 = prompt(parseInt("Enter a number 2"));
+alert (number1, number2);
