3c8b00ed949f64fbd16398a4d787596457b5c828
antony <kiariehjames@gmail.com>
2018-08-16 12:32:04 +0300

    Make changes to index code

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var add = function (number1, number2){
   return(number1 + number2);
 }
-var number1 = prompt(parseInt("Enter a number 1"));
-var number2 = prompt(parseInt("Enter a number 2"));
-alert (number1, number2);
+var number1 = parseInt(prompt("Enter a number 1"));
+var number2 = parseInt(prompt("Enter a number 2"));
+var result = add(number1, number2)
+alert (result);
