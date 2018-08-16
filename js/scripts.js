c954ceb06198bf19c8d76d8847f0ae0c1ab88d8b
Kiarieh <41985453+Kiarieh@users.noreply.github.com>
2018-08-16 10:41:50 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,38 @@
-var add = function (number1, number2){
-  return(number1 + number2);
-}
-var number1 = prompt(parseInt("Enter a number 1"));
-var number2 = prompt(parseInt("Enter a number 2"));
-alert (number1, number2);
+var add = function(number1, number2){
+  return number1 + number2;
+ };
+
+ var number1 = parseInt(prompt("Enter a number to add:"));
+ var number2 = parseInt(prompt("Enter another number:"));
+ var result = add(number1, number2);
+ alert(result);
+
+var subtract = function(number1, number2){
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number to subract:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+
+var divide = function(number1, number2){
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number to divide:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
+
+var multiply = function(number1, number2){
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number to multiply:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
+
+
+
